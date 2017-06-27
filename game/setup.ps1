7573206b21fa76cb408b4292b81eaae85cc672a7
Jeffrey Dwyer <Jeffrey Dwyer>
2017-06-27 07:41:56 -0700

    Revised setup.ps1 to include prompts of what the installer is doing.

--- game/setup.ps1
+++ game/setup.ps1
@@ -1,13 +1,16 @@
+echo 'Installing sdl2 packages...'
 stack exec -- pacman -S mingw-w64-x86_64-pkg-config mingw-w64-x86_64-SDL2 mingw-w64-x86_64-SDL2_mixer
 
+echo 'Cloning library repos...'
 git clone https://github.com/haskell-game/sdl2.git
 git clone https://github.com/sbidin/sdl2-mixer.git
  
+echo 'Building sdl2...'
 cd sdl2
 stack build
 	
+echo "Updating sdl2-mixer's stack.yaml to use local sdl2..."
 cd ..
-
 'flags: {}' >> mixer-stack.yaml
 'extra-package-dbs: []' >> mixer-stack.yaml
 'packages:' >> mixer-stack.yaml
@@ -15,9 +18,11 @@ cd ..
 "- location: ../sdl2" >> mixer-stack.yaml
 'extra-deps: []' >> mixer-stack.yaml
 'resolver: lts-8.18' >> mixer-stack.yaml
-
 rm sdl2-mixer/stack.yaml
 Move-Item mixer-stack.yaml sdl2-mixer/stack.yaml
 
+echo 'Building sdl2-mixer...'
 cd sdl2-mixer
 stack build
+
+echo 'Done!'
